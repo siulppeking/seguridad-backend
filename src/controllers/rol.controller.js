d360fbf2fd5f080a9bae53dd206fd698506fb459
Luis Escobar <dukz@MacBook-Pro-de-Guiseppe.local>
2024-06-20 01:51:14 -0500

    Se agreo el CRUD de roles y el env de desarrollo

--- src/controllers/rol.controller.js
+++ src/controllers/rol.controller.js
@@ -1,39 +1,163 @@
 const Rol = require("../models/rol")
 
 const paginarRoles = async (req, res) => {
-    const roles = await Rol.find({ estado: true });
+    const { pagina = 1, limite = 5 } = req.query;
+    const paginaNumero = parseInt(pagina, 10);
+    const limiteNumero = parseInt(limite, 10);
+    const skip = (paginaNumero - 1) * limiteNumero;
+    const query = {
+        estado: true
+    }
+    const [totalResultados, roles] = await Promise.all([
+        Rol.countDocuments(query),
+        Rol.find(query)
+            .skip(skip).limit(limiteNumero)
+    ]);
+
+    const totalPaginas = Math.ceil(totalResultados / limiteNumero);
+
+    const rolesResponse = roles.map(rol => {
+        return {
+            rolId: rol._id,
+            nombre: rol.nombre
+        }
+    });
 
     return res.status(200).send({
         estado: 'OK',
-        datos: roles
+        respuesta: {
+            totalResultados,
+            totalPaginas,
+            paginaActual: paginaNumero,
+            resultados: rolesResponse
+        }
+
     })
 }
 
 const crearRol = async (req, res) => {
-    const { body: rolBody } = req;
+    try {
+        const { body: rolBody } = req;
+        const regexpNombre = new RegExp(`^${rolBody.nombre}$`, 'i');
 
-    const rolNew = new Rol(rolBody);
-
-    res.status(201).send({
-        estado: 'OK',
-        datos: {
-            mensaje: 'Rol creado correctamente',
-            body: rolNew
+        const rol = await Rol.findOne({ nombre: regexpNombre, estado: true });
+        if (rol) {
+            return res.status(400).send({
+                estado: 'VALIDACION_DB',
+                respuesta: { mensaje: `El nombre ${rolBody.nombre} ya encuentra registrado` }
+            })
         }
-    })
+        const rolData = {
+            nombre: rolBody.nombre
+        }
+        const rolNuevo = new Rol(rolData);
+        const rolCreado = await rolNuevo.save();
+
+        const rolRespuesta = {
+            rolId: rolCreado._id,
+            nombre: rolCreado.nombre
+        }
+
+        return res.status(201).send({
+            estado: 'OK',
+            respuesta: {
+                mensaje: 'Rol creado correctamente',
+                rol: rolRespuesta
+            }
+        })
+    } catch (error) {
+        return res.status(500).send({
+            estado: 'ERROR_EXCEPCION',
+            respuesta: { mensaje: error.message }
+        })
+    }
 
 }
 
 const obtenerRol = async (req, res) => {
+    try {
+        const { rolId } = req.params;
 
+        const rol = await Rol.findOne({ _id: rolId, estado: true });
+        if (!rol) {
+            return res.status(404).send({
+                estado: 'VALIDACION_DB',
+                respuesta: { mensaje: `Id ${rolId} no encontrado` }
+            });
+        }
+        const rolResponse = {
+            rolId: rol._id,
+            nombre: rol.nombre
+        }
+
+        return res.status(200).send({
+            estado: 'OK',
+            respuesta: rolResponse
+        })
+    } catch (error) {
+        return res.status(500).send({
+            estado: 'ERROR_EXCEPCION',
+            respuesta: { mensaje: error.message }
+        })
+    }
 }
 
 const actualizarRol = async (req, res) => {
+    const { rolId } = req.params;
+    const rolEncontrado = await Rol.findOne({ _id: rolId, estado: true });
+    if (!rolEncontrado) {
+        return res.status(404).send({
+            estado: 'VALIDACION_DB',
+            respuesta: { mensaje: `Id ${rolId} no encontrado` }
+        });
+    }
+    const { body: rolBody } = req;
+    const regexpNombre = new RegExp(`^${rolBody.nombre}$`, 'i');
+    const rol = await Rol.findOne({ nombre: regexpNombre, estado: true, _id: { $ne: rolId } })
+    if (rol) {
+        return res.status(400).send({
+            estado: 'VALIDACION_DB',
+            respuesta: { mensaje: `El nombre ${rolBody.nombre} ya encuentra registrado` }
+        })
+    }
+    const rolData = {
+        nombre: rolBody.nombre
+    }
+    const rolAct = await Rol.findByIdAndUpdate(rolId, rolData, { new: true });
 
+    const rolRespuesta = {
+        rolId: rolAct._id,
+        nombre: rolAct.nombre
+    }
+    return res.status(200).send({
+        estado: 'OK',
+        respuesta: {
+            mensaje: 'Rol modificado correctamente',
+            rol: rolRespuesta
+        }
+    })
 }
 
 const eliminarRol = async (req, res) => {
+    const { rolId } = req.params;
+    const rolEncontrado = await Rol.findOne({ _id: rolId, estado: true });
+    if (!rolEncontrado) {
+        return res.status(404).send({
+            estado: 'VALIDACION_DB',
+            respuesta: { mensaje: `Id ${rolId} no encontrado` }
+        });
+    }
+    const rolData = {
+        estado: false
+    }
+    const rolAct = await Rol.findByIdAndUpdate(rolId, rolData, { new: true });
 
+    return res.status(200).send({
+        estado: 'OK',
+        respuesta: {
+            mensaje: 'Rol eliminado correctamente'
+        }
+    })
 }
 
 module.exports = {
--- src/routers/rol.router.js
+++ src/routers/rol.router.js
@@ -5,5 +5,8 @@ const rolRouter = express.Router();
 
 rolRouter.get('/', rolController.paginarRoles);
 rolRouter.post('/', rolController.crearRol);
+rolRouter.get('/:rolId', rolController.obtenerRol);
+rolRouter.put('/:rolId', rolController.actualizarRol);
+rolRouter.delete('/:rolId', rolController.eliminarRol);
 
 module.exports = rolRouter;
